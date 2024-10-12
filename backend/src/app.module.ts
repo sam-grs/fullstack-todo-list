9edd1c65a9813a81c613fe00ded3784380a8de8c
Samira <grossisamira@hotmail.com>
2024-10-12 13:16:11 -0300

    feat: adds AppModule configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { Tasks } from './tasks/entities'
+import { TaskModule } from './tasks/tasks.module'
 
 @Module({
-    imports: [],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'gross',
+            database: 'db_to-do_list',
+            entities: [Tasks],
+            synchronize: true,
+        }),
+        TaskModule,
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
