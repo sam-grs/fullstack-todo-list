71031a66d19b9a34465e63116d8a8a3b9839fe8b
Samira <grossisamira@hotmail.com>
2024-10-12 18:23:02 -0300

    feat: adds bcrypt password

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -4,10 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { Users } from './entities'
 import { UserService } from './services'
 import { UserController } from './controllers'
+import { Bcrypt } from './auth/bcrypt'
 
 @Module({
     imports: [TypeOrmModule.forFeature([Users])],
-    providers: [UserService],
+    providers: [UserService, Bcrypt],
     controllers: [UserController],
     exports: [TypeOrmModule],
 })
