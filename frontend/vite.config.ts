15ffc164b606efc5500d1d291c27eabb42974c1c
Samira <grossisamira@hotmail.com>
2024-10-13 22:04:50 -0300

    feat: adds absolute paths and tailwind with vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            components: path.resolve(__dirname, './src/components'),
+            pages: path.resolve(__dirname, './src/pages'),
+            models: path.resolve(__dirname, './src/models'),
+            services: path.resolve(__dirname, './src/services'),
+            routes: path.resolve(__dirname, './src/routes'),
+        },
+    },
 })
