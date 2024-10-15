9a346fc526144fc56184a579ee31f532999415fc
Samira <grossisamira@hotmail.com>
2024-10-15 12:22:56 -0300

    feat: adds postgres

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,23 +2,18 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { ConfigModule } from '@nestjs/config'
 
-import { Tasks, TaskModule } from './tasks'
-import { User, UserModule, AuthModule } from './users'
+import { TaskModule } from './tasks'
+import { UserModule, AuthModule } from './users'
+import { ProdService } from './services/prod.service'
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
         }),
-        TypeOrmModule.forRoot({
-            type: 'mysql',
-            host: 'localhost',
-            port: 3306,
-            username: 'root',
-            password: 'gross',
-            database: 'db_to_do_list',
-            entities: [Tasks, User],
-            synchronize: true,
+        TypeOrmModule.forRootAsync({
+            useClass: ProdService,
+            imports: [ConfigModule],
         }),
         TaskModule,
         AuthModule,
---
+++ backend/src/services/dev.service.ts
@@ -0,0 +1,21 @@
+﻿import { Injectable } from '@nestjs/common'
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm'
+
+import { Tasks } from 'src/tasks'
+import { User } from 'src/users'
+
+@Injectable()
+export class DevService implements TypeOrmOptionsFactory {
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+        return {
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'gross',
+            database: 'db_to_do_list',
+            entities: [Tasks, User],
+            synchronize: true,
+        }
+    }
+}
